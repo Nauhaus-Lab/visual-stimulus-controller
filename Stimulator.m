604a68729a9603aecd1f3b249ae21dafbbb09598
Ian Nauhaus <inauhaus@gmail.com>
2021-12-08 13:18:35 -0600

    Add files via upload

--- Stimulator.m
+++ Stimulator.m
@@ -3,26 +3,26 @@ function Stimulator
 %%
 
 %Add controller folders to the path
-root = 'C:\stimulus_control\';
-addpath([root 'COM_acquisition'])
-addpath([root 'COM_display'])
-addpath([root 'Calibration'])
-addpath([root 'DisplayCode'])
-addpath([root 'GUIs'])
-addpath([root 'formula functions'])
-addpath([root 'onlineAnalysis'])
-addpath([root 'sync_inputs'])
+root_controller = 'C:\stimulus_control\';
+addpath([root_controller 'COM_acquisition'])
+addpath([root_controller 'COM_display'])
+addpath([root_controller 'Calibration'])
+addpath([root_controller 'DisplayCode'])
+addpath([root_controller 'GUIs'])
+addpath([root_controller 'formula functions'])
+addpath([root_controller 'onlineAnalysis'])
+addpath([root_controller 'sync_inputs'])
 
 %Remove analysis folder from the path
-root = 'C:\ISI acquisition and analysis\';
-rmpath([root 'Processing\AnalysisCode'])
-rmpath([root 'Processing\ISI_Processing'])
-rmpath([root 'Processing\ISIAnGUI'])
-rmpath([root 'Processing\ISIAnGUI\general'])
-rmpath([root 'Processing\AnalysisCode\ContrastResp'])
-rmpath([root 'Processing\AnalysisCode\DynamicProcess'])
-rmpath([root 'Processing\AnalysisCode\DynamicProcess\RevCorr_GUI'])
-rmpath([root 'Processing\offlineMovementCorrection'])
+root_WFanalysis = 'C:\ISI acquisition and analysis\';
+rmpath([root_WFanalysis 'Processing\AnalysisCode'])
+rmpath([root_WFanalysis 'Processing\ISI_Processing'])
+rmpath([root_WFanalysis 'Processing\ISIAnGUI'])
+rmpath([root_WFanalysis 'Processing\ISIAnGUI\general'])
+rmpath([root_WFanalysis 'Processing\AnalysisCode\ContrastResp'])
+rmpath([root_WFanalysis 'Processing\AnalysisCode\DynamicProcess'])
+rmpath([root_WFanalysis 'Processing\AnalysisCode\DynamicProcess\RevCorr_GUI'])
+rmpath([root_WFanalysis 'Processing\offlineMovementCorrection'])
 
 
 %Initialize stimulus parameter structures
